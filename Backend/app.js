85ba8f9b1cfff0208f41baf3e51983b4924194c9
Drew-drashy <drashy21461@iiitd.ac.in>
2025-11-14 16:24:39 +0530

    done-connection-backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import { connectMongo } from "./src/config/mongo.js";
 import videoRoutes from "./src/routes/videoRoutes.js";
-
+import authRoutes from './src/routes/authRoutes.js';
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Mongo connected"))
-  .catch(err => console.error("Mongo error", err));
+await connectMongo();
+
 
 app.use("/api/videos", videoRoutes);
+app.use('/api/auth',authRoutes);
 
 app.get("/", (_, res) => res.send("Backend running 🚀"));
 
--- Backend/src/routes/videoRoutes.js
+++ Backend/src/routes/videoRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { generateVideo, videoWebhook } from "../controllers/videoController.js";
+import { generateVideo, videoWebhook, getVideo,listMyVideos } from "../controllers/videoController.js";
 import { authRequired } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/generate", authRequired, generateVideo);
+router.post('/:id',authRequired,getVideo);
 router.post("/webhook", videoWebhook);
+router.get("/", authRequired, listMyVideos);
 
 export default router;
